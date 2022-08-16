378dc923fbf37071ea44b617223f468dac999a2f
Lipa <ivanlipovtsev@gmail.com>
2022-08-16 23:29:26 +0300

    v2

--- httpd.py
+++ httpd.py
@@ -21,36 +21,25 @@ STATUS_REASON = {
     405: "MethodNotAllowed"
 }
 
-CONTENT_TYPES= {
-    ".txt": "text/plain",
-    ".html": "text/html",
-    ".js": "application/javascript",
-    ".css": "text/css",
-    ".jpeg": "image/jpeg",
-    ".jpg": "image/jpeg",
-    ".png": "image/png",
-    ".gif": "image/gif",
-    ".swf": "application/x-shockwave-flash",
-}
-
 
 class MyHTTPServer:
-    def __init__(self, host, port, workers, 
+    def __init__(self, host, port, workers, doc_root,
                 socket_timeout=SOCKET_TIMEOUT,
                 reconnect_max_attempts=RECONNECT_MAX_ATTEMPTS,
                 reconnect_delay=RECONNECT_DELAY):
         self._host = host
         self._port = port
         self.workers = workers
+        self.doc_root = doc_root
         self.socket_timeout = socket_timeout
         self.reconnect_delay = reconnect_delay
         self.reconnect_max_attempts = reconnect_max_attempts
         self._socket = None
 
 
-    def connect(self):
+    def start_server(self):
         """
-        Метод устанавливки соединения, создает сокет и ждет подключений клиентов,
+        Метод запускает сервер, создает сокет и ждет подключений клиентов,
         масштабируется на несколько воркеров (задается параметром командной строки)
         """
         try:
@@ -59,44 +48,59 @@ class MyHTTPServer:
                 self._socket = socket.socket()
             else:
                 self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self._socket.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
             self._socket.settimeout(self.socket_timeout)
             self._socket.bind((self._host, self._port))
             self._socket.listen()
             for i in range (self.workers):
                 try:
+                    connection = Connection(self._socket)
                     client_handler = threading.Thread(
-                                    target=self.wait_connection,)
+                                    target=connection.wait_connection,)
                     client_handler.start()
                 except Exception as e:
-                    raise Exception(e)
+                    raise Exception(f"Error {e} in connection")
         except socket.error as e:
-            raise Exception(e)
+            raise Exception(f"Error {e} in socket creation")
 
+
+class Connection:
+    """
+    Класс ожидания и создания соединения пользователя с клиентом
+    """
+    def __init__(self, socket):
+        self._socket = socket
 
     def wait_connection(self):
         """
         Метод, в котором принимается соединение от клиента,
-        а также вызываются методы для обработки запроса и создания ответа 
+        а также создаются объекты классов для обработки запроса и создания ответа 
         """
         while True:
             conn, address = self._socket.accept()
             # Обрабатываем запрос
-            method, target, ver, headers = self.read_request(conn)
-            # Генерируем ответ
-            resp = self.make_response(method, target, ver, headers)
-            # Отвечаем клиенту
-            self.send_response(conn, resp)
+            request = Request(conn)
+            method, target, ver, headers = request.read_request()
+            # Генерируем и отправляем ответ
+            send_response = SendResponse(conn, method, target, ver)
+            send_response.make_response()
+            send_response.send_response()
             conn.close()
       
 
-    def read_request(self, conn):
+class Request:
+    """
+    Класс обработки запроса клиента
+    """
+    def __init__(self, connection):
+        self._conn = connection
+
+    def read_request(self):
         """
         Метод, в котором получаем запрос клиента и разбиваем его на параметры 
         (метод запроса, версию протокола, запрашиваемый ресурс 
         и остальные параметры запроса)
         """
-        rfile = conn.makefile('rb')
+        rfile = self._conn.makefile('rb')
         # Работаем с первой строкой запроса
         raw = rfile.readline(MAX_LINE + 1)
         req_line = str(raw, 'iso-8859-1')
@@ -122,14 +126,26 @@ class MyHTTPServer:
         return method, target, ver, headers
 
 
-    def make_response(self, method, target, ver, headers):
-        """
-        Метод, в котором создается ответ на запрос
-        parameters: 
+class SendResponse:
+    """
+    Класс создания ответа и отправки его клиенту
+    parameters: 
+            - connection - клиентское подключение
             - method - метод запроса
             - target - запрашиваемый ресурс
             - ver - версию протокола
-            - headers - словарь остальных параметров запроса
+            - resp - объект класса Response (сформированный ответ клиенту)
+    """
+    def __init__(self, connection, method, target, ver):
+            self._conn = connection
+            self.method = method
+            self.target = target
+            self.ver = ver
+            self.resp = None
+
+    def make_response(self):
+        """
+        Метод создания ответа на запрос клиента
         """
         status = 200
         headers = {}
@@ -137,17 +153,14 @@ class MyHTTPServer:
         headers['Server'] = "My_Super_Server"
         now = datetime.now()
         headers['Date'] = now.strftime("%m/%d/%Y, %H:%M:%S")
-        if method not in ['GET', 'HEAD'] or ver != 'HTTP/1.1':
+        if self.method not in ['GET', 'HEAD'] or self.ver != 'HTTP/1.1':
             status = 405
-            resp = Response(status, STATUS_REASON[status], headers)
-            return resp
-        file_path = opts.doc_root+target
+            self.resp = Response(status, STATUS_REASON[status], headers)
+            return self.resp
         # если в запрашивамом ресурсе клиент пытаеься выйти из директории проекта
-        # (методом \..\..\..\)
-        if re.findall(r'\/\.\.\/', target):
-            status = 404
-            resp = Response(status, STATUS_REASON[status], headers)
-            return resp
+        # что "схлопываем все ../"
+        target = self.target.replace('../', '')
+        file_path = opts.doc_root+target
         # Если запрашивается директория - возвращаем файл index.html
         if Path(file_path).is_dir():
             file_path+='index.html'
@@ -155,53 +168,50 @@ class MyHTTPServer:
             # Если после запрашиваемого файла клиент поставил /
             if target[len(target)-1] == '/':
                 status = 404
-                resp = Response(status, STATUS_REASON[status], headers)
-                return resp
-        # Считываем данные из запрашиваемого файла
-        try:
-            file = open(file_path, 'rb')
-        except:
-            status = 404
-            resp = Response(status, STATUS_REASON[status], headers)
-            return resp
-        data = b''
-        for line in file:
-            data+=line
-            data+=b'\n'
-        # Считаем размер файла, а также определяем его тип
-        headers['Content-Length'] = os.stat(file_path).st_size
-        mime = magic.Magic(mime=True)
-        file_type = Path(file_path).suffix
-        headers['Content-Type'] = CONTENT_TYPES[file_type]
-        if method == "GET":
-            resp = Response(status, STATUS_REASON[status], headers, data)
-        if method == "HEAD":
-            resp = Response(status, STATUS_REASON[status], headers)
-        return resp
+                self.resp = Response(status, STATUS_REASON[status], headers)
+                return self.resp
+        # Проверяем метод запроса и выбираем, считывать ли файл
+        if self.method == "GET":
+            # Считываем данные из запрашиваемого файла
+            try:
+                file = open(file_path, 'rb')
+                # Считаем размер файла, а также определяем его тип
+                headers['Content-Length'] = os.stat(file_path).st_size
+                headers['Content-Type'] = mimetypes.guess_type(file_path)[0]
+            except:
+                status = 404
+                self.resp = Response(status, STATUS_REASON[status], headers)
+                return self.resp
+            data = file.read() 
+            self.resp = Response(status, STATUS_REASON[status], headers, data)
+        if self.method == "HEAD":
+            self.resp = Response(status, STATUS_REASON[status], headers)
+        return self.resp
 
 
-    def send_response(self, conn, resp):
+    def send_response(self):
         """
-        Метод, в котором отправляем запрос
-        parameters: 
-            - conn - коннектор подключения клиента
-            - resp - объект класса Response (сформарованный ответ клиенту)
+        Метод отправки запроса
+            
         """
-        wfile = conn.makefile('wb')
-        status_line = f'HTTP/1.1 {resp.status} {resp.reason}\r\n'
+        wfile = self._conn.makefile('wb')
+        status_line = f'HTTP/1.1 {self.resp.status} {self.resp.reason}\r\n'
         wfile.write(status_line.encode('iso-8859-1'))
-        if resp.headers:
-            for key, value in resp.headers.items():
+        if self.resp.headers:
+            for key, value in self.resp.headers.items():
                 header_line = f'{key}: {value}\r\n'
                 wfile.write(header_line.encode('iso-8859-1'))
         wfile.write(b'\r\n')
-        if resp.body:
-            wfile.write(resp.body)
+        if self.resp.body:
+            wfile.write(self.resp.body)
         wfile.flush()
         wfile.close()
 
 
 class Response():
+    """
+    Класс описывает параметры ответа сервера
+    """
     def __init__(self, status, reason, headers=None, body=None):
         self.status = status
         self.reason = reason
@@ -217,8 +227,8 @@ if __name__ == '__main__':
     op.add_option("-u", "--url", action="store", default='localhost')
     op.add_option("-w", "--workers", action="store", default=1)
     (opts, args) = op.parse_args()
-    serv = MyHTTPServer(opts.host, opts.port, opts.workers)
+    serv = MyHTTPServer(opts.host, opts.port, opts.workers, opts.doc_root)
     try:
-        serv.connect()
+        serv.start_server()
     except KeyboardInterrupt:
         pass
